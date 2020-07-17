b8f7f5abbdb566a7c6a0fd7766cb32a7f9ee363d
kavinda <kavindasr@icloud.com>
2020-07-17 12:32:33 +0530

    Revert "error"
    This reverts commit de9473fdbee00a84fbc7ce9ca327a11de47a4eb6.

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const host = process.env.HOST;
+const host = process.env.Host;
 const webport = process.env.WEBPORT;
 const protocol = process.env.PROTOCOL;
 const connectionLimit = process.env.CONNECTIONLIMIT;
@@ -8,6 +8,7 @@ const user = process.env.USER;
 const password = process.env.PASSWORD;
 const database = process.env.DATABASE;
 
+console.log(host);
 
 exports.webSettings  = {
     webport,
