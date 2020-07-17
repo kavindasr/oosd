ff231ce15c1b69263205b92f176bb90dd2ce62c8
kavinda <kavindasr@icloud.com>
2020-07-17 12:47:18 +0530

    db is working fine

--- db/db.js
+++ db/db.js
@@ -2,13 +2,13 @@ var mysql = require('mysql');
 const {dbConfig} = require('../settings');
 var pool  = mysql.createPool(dbConfig);
  
-exports.executeSQL = (sql)=>{
+function executeSQL(sql){
     return new Promise((res,rej)=>{
         pool.getConnection(function(err, connection) {
             if (err) throw err; // not connected!
            
             // Use the connection
-            connection.query('SELECT something FROM sometable', function (error, results, fields) {
+            connection.query(sql, function (error, results, fields) {
 
                 // When done with the connection, release it.
                 connection.release();
@@ -24,3 +24,5 @@ exports.executeSQL = (sql)=>{
           });
     });
 }
+
+module.exports = {executeSQL};
