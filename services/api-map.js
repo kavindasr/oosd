9848df7cdf761e950225d6b89ebe7cc9e3038131
kavinda <kavindasr@icloud.com>
2020-07-23 20:34:05 +0530

    path to database mapping

---
+++ services/api-map.js
@@ -0,0 +1,36 @@
+
+const table = new Map(
+    [
+        ['employee','employee_table'],
+        ['attendance','daily_attendance'],
+        ['division','division_table'],
+        ['division','vehicle_detail']
+    ]
+);
+
+const condition = new Map(
+    [
+        ['id','employee_id'],
+        ['date','tdate'],
+        ['div','division']
+    ]
+);
+
+const field = new Map(
+    [
+        ['all','*'],
+        //add unique key to represnt deferent combinations of fields
+    ]
+);
+
+const getTable = (key)=>{
+    return table.get(key);
+}
+const getConditon = (key)=>{
+    return condition.get(key);
+}
+const getField = (key)=>{
+    return field.get(key);
+}
+
+module.exports = {getTable,getConditon,getField};
