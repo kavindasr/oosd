38d4be630e49dd744322ea2f8cbbe8adb74210c8
Isuru Ariyarathne <61895498+IsuruAriyarathne@users.noreply.github.com>
2020-08-11 14:26:37 +0530

    put and delete method completed

--- methods/apiMethod.js
+++ methods/apiMethod.js
@@ -1,111 +1,130 @@
-const  {getTable,getConditon,getField} = require('../services/api-map');
-const {executeSQL} = require('../db/db');
-const {Send500,SendJson} = require('../responses/response');
+const { getTable, getConditon, getField } = require("../services/api-map");
+const { executeSQL } = require("../db/db");
+const { Send500, SendJson } = require("../responses/response");
 
-class ApiMethod{ 
-    constructor(method){
-        this.query = null ;
-        this.method = method;
+class ApiMethod {
+  constructor(method) {
+    this.query = null;
+    this.method = method;
+  }
+  get getQuery() {
+    return this.query;
+  }
+  setFeilds() {
+    const fieldsList = this.method.getPath(3).split("&");
+    var arr = [];
+    for (var element of fieldsList) {
+      arr.push(getField(element));
     }
-    get getQuery(){
-        return this.query;
-    }
-    setFeilds(){
-        const fieldsList = this.method.getPath(3).split('&');
-        var arr = [];
-        for (var element of fieldsList){
-            arr.push(getField(element));
-        }
-        return arr.join();
-    }
+    return arr.join();
+  }
 
-    setConditions(){
-        var conditionQ=[];
-        var part ="";
-        this.method.url.searchParams.forEach((value, name, searchParams) => {
-            part = `${getConditon(name)}=${value}`
-            conditionQ.push(part);
-        });
-        return conditionQ.join(" AND ");
-        //console.log(conditionQ.join());
-    }
-    async jsBody(){
-        const reqBody = await this.method.getBody();
-        var fieldStr='';
-        var valueStr='';
-        var data = JSON.parse(reqBody);
+  setConditions() {
+    var conditionQ = [];
+    var part = "";
+    this.method.url.searchParams.forEach((value, name, searchParams) => {
+      part = `${getConditon(name)}=${value}`;
+      conditionQ.push(part);
+    });
+    return conditionQ.join(" AND ");
+    //console.log(conditionQ.join());
+  }
+  async jsBody() {
+    const reqBody = await this.method.getBody();
+    var fieldStr = "";
+    var valueStr = "";
+    var data = JSON.parse(reqBody);
 
-        for (let key in data){
-
-            fieldStr = fieldStr + getField(key) + ",";
-            valueStr = valueStr  + data[key] + ",";
-        } 
-        return({"field":fieldStr.slice(0,-1),"val":valueStr.slice(0,-1)});
+    for (let key in data) {
+      fieldStr = fieldStr + getField(key) + ",";
+      valueStr = valueStr + data[key] + ",";
     }
-    async execute(){
-        try{
-            
-            const data =  await executeSQL(this.query);
-            return new SendJson(JSON.stringify(data));
-            
-        }catch(e){ 
-            //return new Send500(e);
-        }
+    return { field: fieldStr.slice(0, -1), val: valueStr.slice(0, -1) };
+  }
+  async execute() {
+    try {
+      const data = await executeSQL(this.query);
+      return new SendJson(JSON.stringify(data));
+    } catch (e) {
+      //return new Send500(e);
     }
-
+  }
 }
 
-class ApiGet extends ApiMethod{
-    constructor(method){
-        super(method);
+class ApiGet extends ApiMethod {
+  constructor(method) {
+    super(method);
+  }
+  setQuery() {
+    const fields = this.setFeilds();
+    const condition = this.setConditions();
+    if (condition) {
+      this.query = `SELECT ${fields} FROM ${getTable(
+        this.method.getPath(2)
+      )} WHERE ${condition}`;
+    } else {
+      this.query = `SELECT ${fields} FROM ${getTable(this.method.getPath(2))}`;
     }
-    setQuery(){
-        const fields = this.setFeilds();
-        const condition = this.setConditions();
-        if(condition){
-            this.query = `SELECT ${fields} FROM ${getTable(this.method.getPath(2))} WHERE ${condition}`;
-        }
-        else{
-            this.query = `SELECT ${fields} FROM ${getTable(this.method.getPath(2))}`;
-        }
-        
-    }
+  }
 }
 
-class ApiPost extends ApiMethod{
-    constructor(method){
-        super(method);
-    }
-    async setQuery(){
+class ApiPost extends ApiMethod {
+  constructor(method) {
+    super(method);
+  }
+  async setQuery() {
+    const StrComp = await this.jsBody();
+    const fields = StrComp["field"];
+    const values = StrComp["val"];
 
-        const StrComp = await this.jsBody();
-        const fields = StrComp["field"];
-        const values = StrComp["val"];
-
-        this.query = `INSERT INTO ${getTable(this.method.getPath(2))} (${fields}) VALUES (${values})`;
-        return(true);
-        
-    }
+    this.query = `INSERT INTO ${getTable(
+      this.method.getPath(2)
+    )} (${fields}) VALUES (${values})`;
+    return true;
+  }
 }
 
-class ApiPut extends ApiMethod{
-    constructor(method){
-        super(method);
+class ApiPut extends ApiMethod {
+  constructor(method) {
+    super(method);
+  }
+
+  async setQuery() {
+    const StrComp = await this.jsBody();
+    var fieldsQ = [];
+    const Fields = StrComp["field"];
+    const fields = Fields.split(",");
+    const Values = StrComp["val"];
+    const values = Values.split(",");
+    const condition = this.setConditions();
+
+    var i;
+    var fieldStr = "";
+    var fieldstr = "";
+    for (i = 0; i < fields.length; i++) {
+      fieldStr = fields[i] + '="' + values[i] + '"';
+      fieldsQ.push(fieldStr);
     }
-    setQuery(table,fields,conditions){
-        this.query = `UPDATE ${table} SET ${fields} WHERE ${conditions}`;
-    }
+
+    fieldstr = fieldsQ.join(",");
+
+    this.query = `UPDATE ${getTable(
+      this.method.getPath(2)
+    )} SET ${fieldstr} WHERE ${condition}`;
+  }
 }
 
-class ApiDelete extends ApiMethod{
-    constructor(method){
-        super(method);
-    }
+class ApiDelete extends ApiMethod {
+  constructor(method) {
+    super(method);
+  }
 
-    setQuery(table,conditions){
-        this.query = `DELETE FROM ${table} WHERE ${conditions}`;
-    }
+  async setQuery() {
+    const condition = this.setConditions();
+    this.query = `DELETE FROM ${getTable(
+      this.method.getPath(2)
+    )} WHERE ${condition}`;
+  }
 }
 
-
-module.exports = {ApiGet,ApiPost,ApiPut,ApiDelete};
+module.exports = { ApiGet, ApiPost, ApiPut, ApiDelete };
