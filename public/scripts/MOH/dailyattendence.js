543f420de7f69ae787c2240b3a98e98482547c7e
Isuru Ariyarathne <61895498+IsuruAriyarathne@users.noreply.github.com>
2020-09-06 17:26:53 +0530

    js files added

--- public/scripts/MOH/dailyattendence.js
+++ public/scripts/MOH/dailyattendence.js
@@ -1,25 +1,18 @@
+
 function viewEmployee() {
+function viewattendence() {
+  console.log("hiiiis");
   var xhttp = new XMLHttpRequest();
   var url =
     'http://localhost:8000/api/attendance/all?date="' +
     document.getElementById("inpdate").value +
     '"';
+    'http://localhost:8000/api/attendance/all?date=' +
+    document.getElementById("inpdate").value;
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       var empdetails = JSON.parse(this.responseText);
+      console.log(empdetails);
       renderHtml(empdetails);
     }
-  };
-  xhttp.open("GET", url, true);
-  xhttp.send();
-}
-
-function renderHtml(data) {
-  document.getElementById("empID").innerHTML =
-    "Employee ID  : " + data[0].employee_id;
-  document.getElementById("empdiv").innerHTML =
-    "Division  : " + data[0].division;
-  document.getElementById("entdate").innerHTML = "date  : " + data[0].tdate;
-  document.getElementById("vehiclewalk").innerHTML =
-    "vehiclewalk  : " + data[0].vehiclewalk;
 }
