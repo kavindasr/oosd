9a211208ccc04df9151fbbd245d40f01acbd6386
roshinie <roshinie.18@cse.mrt.ac.lk>
2020-09-06 23:07:19 +0530

    Revert "moh js added"
    This reverts commit 23a52dfdcabea6cf5f8fc0ed0ed991f060d0ec81.

--- services/api-map.js
+++ services/api-map.js
@@ -1,77 +1,75 @@
 
 const table = new Map(
     [
-        ['employee', 'employee_table'],
-        ['attendance', 'daily_attendance'],
-        ['division', 'division_table'],
-        ['vehicle', 'vehicle_detail'],
-        ['session', 'session_table'],
-        ['gunbill', 'gin_unbilled'],
-        ['ginbill', 'gin_billed'],
-        ['gdetail', 'gtype_detail'],
-        ['gout', 'garbage_out'],
+        ['employee','employee_table'],
+        ['attendance','daily_attendance'],
+        ['division','division_table'],
+        ['vehicle','vehicle_detail'],
+        ['session','session_table'],
+        ['gunbill','gin_unbilled'],
+        ['ginbill','gin_billed'],
+        ['gdetail','gtype_detail'],
+        ['gout' , 'garbage_out'],
         ['compin', 'compost_in'],
-        ['compout', 'compost_out'],
-        ['user', 'user_table']
+        ['compout', 'compost_out']
     ]
 );
 
 const condition = new Map(
     [
-        ['empid', 'employee_id'], //id to empid
-        ['date', 'tdate'],
-        ['div', 'division'],
-        ['divno', 'division_no'],
-        ['gtype', 'waste_type'],
-        ['salid', 'salary_id'],
-        ['gID', 'gindex'],
-        ['userName', 'user_name']
+        ['empid','employee_id'], //id to empid
+        ['date','tdate'],
+        ['div','division'],
+        ['divno','division_no'],
+        ['gtype','waste_type'],
+        ['salid','salary_id'],
+        ['gID','gindex']
     ]
 );
 
 const field = new Map(
     [
-        ['all', '*'],
-        ['empId', 'employee_id'],
-        ['salId', 'salary_id'],
-        ['empName', 'name'],
+        ['all','*'],
+        ['empId','employee_id'],
+        ['salId','salary_id'],
+        ['empName','name'],
         ['bdate', 'dob'],
         ['empType', 'employee_type'],
-        ['sex', 'gender'],
-        ['date', 'tdate'],
+        ['sex','gender'],
+        ['date','tdate'],
         ['div', 'division'],
-        ['divName', 'division_name'],
-        ['mode', 'vehiclewalk'],
-        ['gID', 'gindex'],
-        ['gtype', 'waste_type'],
-        ['unitp', 'unit_price'],
+        ['divName','division_name'],
+        ['mode','vehiclewalk'],
+        ['gID','gindex'],
+        ['gtype','waste_type'],
+        ['unitp','unit_price'],
         //ginbill
-        ['inday', 'in_date'],
-        ['time', 'in_time'],
-        ['gtypo', 'g_type'],
-        ['weight', 'in_weight'],
-        ['amnt', 'bill_amount'],
+        ['inday','in_date'],
+        ['time','in_time'],
+        ['gtypo','g_type'],
+        ['weight','in_weight'],
+        ['amnt','bill_amount'],
         //gout
-        ['oday', 'out_date'],
-        ['otime', 'out_time'],
-        ['oweight', 'weight'],
-        ['invoice', 'invoice_no'],
+        ['oday','out_date'],
+        ['otime','out_time'],
+        ['oweight','weight'],
+        ['invoice','invoice_no'],
         //compost
-        ['pctin', 'pct_produced'],
-        ['pctout', 'pct_sold'],
-        ['maxid', 'MAX(invoice_no) AS pr']
+        ['pctin','pct_produced'],
+        ['pctout','pct_sold'],
+        ['maxid','MAX(invoice_no) AS pr']
         //add unique key to represnt different combinations of fields
     ]
 );
 
-const getTable = (key) => {
+const getTable = (key)=>{
     return table.get(key);
 }
-const getConditon = (key) => {
+const getConditon = (key)=>{
     return condition.get(key);
 }
-const getField = (key) => {
+const getField = (key)=>{
     return field.get(key);
 }
 
-module.exports = { getTable, getConditon, getField };
+module.exports = {getTable,getConditon,getField};
