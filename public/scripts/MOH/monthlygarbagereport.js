e31fbca4fe1d491af90a0810fa4e95240b58bbde
Isuru Ariyarathne <61895498+IsuruAriyarathne@users.noreply.github.com>
2020-09-13 16:24:13 +0530

    to check endpoints

---
+++ public/scripts/MOH/monthlygarbagereport.js
@@ -0,0 +1,62 @@
+var d = new Date();
+
+
+function monthlyginbilledincome() {
+    console.log("hiiiiiii");
+    if(document.getElementById("adduserselection").value=="august"){
+        month=08;
+        days=31;
+    }
+    
+    var xhttp = new XMLHttpRequest();
+    var url ='http://localhost:8000/report/dRange/unbilled?sDate=' +d.getFullYear() +"-" +month+"-"+"01&eDate="+d.getFullYear() +"-" +month+"-"+days;
+    console.log(url);
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        var ginchartdetails = JSON.parse(this.responseText);
+        console.log(ginchartdetails);
+        renerginbilledchart(ginchartdetails);
+      }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+  }
+  
+  function renerginbilledchart(ginchartdetails){
+    google.charts.load('current', {'packages':['bar']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ['Date', 'Degradable', 'Non-Degradable'],
+        ['Monday', 1000, 400],
+        ['Tuesday', 200 , 190],
+        ['Wednesday', 660, 800],
+        ['Thursday', 103, 540],
+        ['Friday', 117, 0],
+        ['Saturday', 66, 112],
+        ['Sunday', 103, 54]
+      ]);
+
+      var options = {
+          chart: {
+              title: 'Garbage Received Unbilled',
+              subtitle: 'Degradable, Non-Degradable: Weekly Report',
+          },
+          chartArea: {
+              backgroundColor: {
+              fill: '#d6e0f0',
+              fillOpacity: 0.1
+              },
+          },
+          backgroundColor: {
+              fill: '#d6e0f0',
+          },
+      };
+
+      var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+      chart.draw(data, google.charts.Bar.convertOptions(options));
+  }
+}
+  
