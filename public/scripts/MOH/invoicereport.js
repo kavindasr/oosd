67b7005cef927dd4b330333051a2e2cc0cf62887
Isuru Ariyarathne <61895498+IsuruAriyarathne@users.noreply.github.com>
2020-09-27 12:46:44 +0530

    invoice completed

--- public/scripts/MOH/invoicereport.js
+++ public/scripts/MOH/invoicereport.js
@@ -16,11 +16,61 @@ function getinvoicegarbageout() {
 function rendergarbageout(arr){
     console.log(arr);
     document.getElementById("myModal2").style.display ="block";
-    document.getElementById("invoice1").innerHTML = arr[0].invoice_no;
+    document.getElementById("invoice2").innerHTML = arr[0].invoice_no;
+    document.getElementById("date2").innerHTML = arr[0].out_date;
+    document.getElementById("time2").innerHTML = arr[0].out_time;
+    document.getElementById("gartype2").innerHTML = arr[0].waste_type;
+    document.getElementById("wght2").innerHTML = arr[0].weight;
+}
+
+function getinvoicegarbagein() {
+    var invoice_num = document.getElementById("invoicenum").value;
+    var xhttp = new XMLHttpRequest();
+    var url ="http://localhost:8000/api/ginbill/all?invoice=" +invoice_num;
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var empdetails = JSON.parse(this.responseText);
+            rendergarbagein(empdetails);
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+function rendergarbagein(arr){
     console.log(arr[0]);
-    document.getElementById("date1").innerHTML = arr[0].out_date;
-    console.log(arr.date);
-    document.getElementById("time1").innerHTML = arr[0].out_time;
-    document.getElementById("gartype1").innerHTML = arr[0].waste_type;
-    document.getElementById("wght1").innerHTML = arr[0].weight;
+    document.getElementById("myModal1").style.display ="block";
+    document.getElementById("invoice1").innerHTML = arr[0].invoice_no;
+    document.getElementById("date1").innerHTML = arr[0].in_date;
+    document.getElementById("time1").innerHTML = arr[0].in_time;
+    document.getElementById("gartype1").innerHTML = arr[0].g_type;
+    document.getElementById("wght1").innerHTML = arr[0].in_weight;
+    document.getElementById("amnt1").innerHTML = arr[0].bill_amount;
+}
+
+function getinvoicecompost() {
+    var invoice_num = document.getElementById("invoicenum").value;
+    var xhttp = new XMLHttpRequest();
+    var url ="http://localhost:8000/api/compout/all?invoice=" +invoice_num;
+    console.log(url);
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var empdetails = JSON.parse(this.responseText);
+            rendercompost(empdetails);
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+function rendercompost(arr){
+    console.log(arr[0]);
+    document.getElementById("myModal3").style.display ="block";
+    document.getElementById("invoice3").innerHTML = arr[0].invoice_no;
+    document.getElementById("date3").innerHTML = arr[0].out_date;
+    document.getElementById("time3").innerHTML = arr[0].out_time;
+    document.getElementById("pct3").innerHTML = arr[0].pct_solid;
+    document.getElementById("amnt3").innerHTML = arr[0].bill_amount;
 }
