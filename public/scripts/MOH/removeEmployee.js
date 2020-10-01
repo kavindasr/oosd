5a3e77d604d766320db9c2eaf4e26161da87f696
Isuru Ariyarathne <61895498+IsuruAriyarathne@users.noreply.github.com>
2020-10-01 19:04:35 +0530

    missed

--- public/scripts/MOH/removeEmployee.js
+++ public/scripts/MOH/removeEmployee.js
@@ -1,14 +1,32 @@
-function removeemp() {
-  console.log("hiiii");
-  var url ='http://localhost:8000/api/employee?empid=' +document.getElementById("removeid").value;
-  console.log(url);
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      alert("employee removed successfully");
-      window.location.reload();
+// function removeemp() {
+//   var url ='http://localhost:8000/api/employee?empid=' +document.getElementById("removeid").value;
+//   var xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function () {
+//     if (this.readyState == 4 && this.status == 200) {
+//       alert("employee removed successfully");
+//       window.location.reload();
+//     }
+//   };s
+//   xhttp.open("DELETE", url, true);
+//   xhttp.send();
+// }
+
+async function removeemp(){
+  var empid = document.getElementById("removeid").value;
+  var validate;
+    try{
+        validate = await apiCall("DELETE",`${domain}/api/employee?empid=${empid}`);
+        console.log(validate);
     }
-  };
-  xhttp.open("DELETE", url, true);
-  xhttp.send();
+    catch(e){
+        validate = e ;
+    }
+    if(validate == "Ok"){
+      
+        alert("Try Again");
+
+    }
+    else{
+        alert("Invalid employee ID");
+    }
 }
