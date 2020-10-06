ba9ccfe85f73a835de1dc31dad416aae2a7d4f71
Isuru Ariyarathne <61895498+IsuruAriyarathne@users.noreply.github.com>
2020-10-06 20:17:08 +0530

    add user

--- public/scripts/MOH/addUser.js
+++ public/scripts/MOH/addUser.js
@@ -1,22 +1,27 @@
 function addusernew() {
     var emp_Name = document.getElementById("addusername").value;
     var emp_type = (document.getElementById("adduserselection").value);
-    var pswd = (document.getElementById("adduserpassword").value);
+    var pswd1 = (document.getElementById("adduserpassword12").value);
+    var pswd2 = (document.getElementById("adduserpassword34").value);
 
 
     var userObj = {
-        password: pswd,
+        password: pswd1,
         userName: emp_Name,
         userType: emp_type,
     };
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            alert("User Added Successfully..");
-            window.location.reload();
-        }
-    };
-    xhttp.open("POST", "http://localhost:8000/signup", true);
-    xhttp.send(JSON.stringify(userObj));
+    if (pswd1==pswd2){
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                alert("User Added Successfully..");
+                window.location.reload();
+            }
+        };
+        xhttp.open("POST", "http://localhost:8000/signup", true);
+        xhttp.send(JSON.stringify(userObj));
+    }else{
+        alert("paswor mismatch")
+    }
 }
